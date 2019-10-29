23f48cee1f24f6976dac2aa1af717e786289dce7
Carlson Kamau <carlsonkamau@gmail.com>
2019-10-29 19:56:55 +0300

    Second commit 29/10/19

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="description-body">
           <Navbar />
           <Description />
         </div>
